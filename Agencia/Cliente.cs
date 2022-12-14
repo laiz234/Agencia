59253ee7c5f15dec4be04d25ffe3df69b84717e5
LAIZ FERNANDA SOUZA DOS SANTOS <laiz.santos@academico.ifrn.edu.br>
2022-12-14 14:08:11 -0300

    Adicionar arquivos de projeto.

---
+++ Agencia/Cliente.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agencia
+{
+    class Cliente
+    {
+        private string nome;
+        private string cpf;
+        private Conta[] contas = new Conta[1000];
+        private int k = 0;
+        private double saldo;
+        public void Inserir(Conta c)
+        {
+            contas[k] = c;
+            k++;
+        }
+        public Conta[] Listar()
+        {
+            Conta[] vetor = new Conta[k];
+            Array.Copy(contas, vetor, k);
+            return vetor;
+        }
+        public double SaldoTotal()
+        {
+            foreach (Conta x in contas)
+                SaldoTotal += saldo;
+            return SaldoTotal;
+        }
+    }
+
+}
---
+++ Agencia/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agencia
+{
+    class Conta
+    {
+        private string numero;
+        private string agencia;
+        private double saldo;
+
+        public object Saldo { get; internal set; }
+
+        public void SetNumero(string n)
+        {
+            numero = n;
+        }
+        public void SetAgencia(string a)
+        {
+            agencia = a;
+        }
+        public void SetSaldo(double s)
+        {
+            if (s >= 0) saldo = s;
+        }
+        public string GetNumero()
+        {
+            return numero;
+        }
+        public string GetAutores()
+        {
+            return agencia;
+        }
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+        public override string ToString()
+        {
+            if (saldo == 0)
+                return $"{numero} - {agencia} - {saldo} sem saldo! ";
+            return $"{numero} - {agencia} - {saldo} ";
+        }
+    }
+}
